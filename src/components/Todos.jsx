4b63c0a2609e01fd5134974efd6cdf136f42c441
michael <michaelalexander6666@outlook.com>
2024-06-01 21:08:43 +0700

    Add CSS to components

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import TodoItem from './TodoItem'
 
+const style = { 
+	width: '40%', 
+	margin: '0 auto' 
+}
+
+
 const Todos = ({ todos }) => {
   return (
-		<div>
+		<div style={style}>
 			{todos.map((todo) => {
         return <TodoItem key={todo.id} todo={todo} />
       })}
